1ab7618f6c784523e0607f4cac2cde7db59f9e85
gksrlfw <hankil.seo@solarconnect.kr>
2021-08-05 19:05:26 +0900

    grpc test

---
+++ grpc-client/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  
+  @Get()
+  call(): Observable<any> {
+    console.log('client!!');
+    return this.appService.getSampleData();
+  }
+}
---
+++ grpc-server/src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
+import { SampleData, SampleDataById } from './model';
+
+@Controller()
+export class AppController {
+  /**
+   * AppService findOne
+   * @param data id
+   * @param metadata metadata
+   * @returns SampleData
+   */
+  @GrpcMethod('AppService')
+  findOne(data: SampleDataById, metadata: any): SampleData {
+    console.log('server!!');
+    const items = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+    ] as SampleData[];
+    const filteredItems = items.filter((item) => item.id === data.id);
+    return filteredItems.length > 0 ? filteredItems[0] : ({} as SampleData);
+  }
+}
